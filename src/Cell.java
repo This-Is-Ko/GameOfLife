2d4d5faa459a31af2130c75adf9c8decb92de491
This-Is-Ko <52279273+This-Is-Ko@users.noreply.github.com>
2023-09-14 18:39:58 +1000

    Updated field to final

--- src/Cell.java
+++ src/Cell.java
@@ -2,8 +2,8 @@ package src;
 
 public class Cell {
 
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Cell(int x, int y) {
         this.x = x;
