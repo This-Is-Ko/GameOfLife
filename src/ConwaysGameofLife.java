5e2994d53ad114a251c5c4955eff91f350e8001d
This-Is-Ko <52279273+This-Is-Ko@users.noreply.github.com>
2023-09-14 18:35:33 +1000

    Updated input handling

--- src/ConwaysGameofLife.java
+++ src/ConwaysGameofLife.java
@@ -6,13 +6,14 @@ import java.util.regex.Pattern;
 
 public class ConwaysGameofLife {
 
+    private static final String INPUT_REGEX = "\\[(-?\\d+), (-?\\d+)\\]";
     private static final int MAX_GENERATIONS = 100;
     private static final int GRID_LENGTH = 200;
     private static final int GRID_WIDTH = 200;
 
     private static final int[][] NEIGHBOUR_OFFSETS = {
-            {-1, 1},   {0, 1},  {1, 1},
-            {-1, 0},            {1, 0},
+            {-1, 1}, {0, 1}, {1, 1},
+            {-1, 0}, {1, 0},
             {-1, -1,}, {0, -1}, {1, -1}
     };
 
@@ -22,30 +23,22 @@ public class ConwaysGameofLife {
         if (args.length == 1) {
             // Extract cell coordinates from the input string using regex
             // Expected format [[x1, y1], [x2, y2]]
-            String input = args[0];
-            // Handle any unexpected whitespace in input
-            input = input.replaceAll("\\s", "");
-            Pattern pattern = Pattern.compile("\\[(\\d+),(\\d+)\\]");
-            Matcher matcher = pattern.matcher(input);
-
-            while (matcher.find()) {
-                int x = Integer.parseInt(matcher.group(1));
-                int y = Integer.parseInt(matcher.group(2));
-                initPopulatedCells.add(new Cell(x, y));
-            }
+            parseInput(initPopulatedCells, args[0]);
         } else {
             System.err.println("Invalid input. Example usage: java ConwaysGameofLife \"[[x1, y1], [x2, y2], ...]\"");
             System.exit(1);
         }
 
+
+        validateInput(initPopulatedCells, GRID_LENGTH, GRID_WIDTH);
+
         gameOfLife(initPopulatedCells, MAX_GENERATIONS, GRID_LENGTH, GRID_WIDTH);
     }
 
-    private static void gameOfLife(ArrayList<Cell> populatedCells, int maxGenerations, int gridLength, int gridWidth){
+    private static void gameOfLife(ArrayList<Cell> populatedCells, int maxGenerations, int gridLength, int gridWidth) {
         ArrayList<Cell> currentGenCells = populatedCells;
 
         for (int generation = 1; generation <= maxGenerations; generation++) {
-            // TODO Change to foreach or stream
             // Generate behaviour of populated cells
             ArrayList<Cell> nextGenCells = new ArrayList<>();
             for (Cell cell : currentGenCells) {
@@ -88,7 +81,6 @@ public class ConwaysGameofLife {
         // (x,y) offset
         int populatedNeighbours = 0;
 
-        // TODO Change to nested for loops
         for (int[] neighbourOffset : NEIGHBOUR_OFFSETS) {
             int neighbourX = cell.getX() + neighbourOffset[0]; // x change
             int neighbourY = cell.getY() + neighbourOffset[1]; // y change
@@ -97,5 +89,32 @@ public class ConwaysGameofLife {
             }
         }
         return populatedNeighbours;
+    }
+
+
+    private static void parseInput(ArrayList<Cell> initPopulatedCells, String input) {
+        Pattern pattern = Pattern.compile(INPUT_REGEX);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            int x = Integer.parseInt(matcher.group(1));
+            int y = Integer.parseInt(matcher.group(2));
+            initPopulatedCells.add(new Cell(x, y));
+        }
+    }
+
+    private static void validateInput(ArrayList<Cell> initPopulatedCells, int gridLength, int gridWidth) {
+        // Check at least one cell is populated
+        if (initPopulatedCells.isEmpty()) {
+            System.err.println("Invalid input. Example usage: java ConwaysGameofLife \"[[x1, y1], [x2, y2], ...]\"");
+            System.exit(1);
+        }
+        // Ensure input is within grid
+        for (Cell cell : initPopulatedCells) {
+            if (cell.getX() < 0 || cell.getX() > gridWidth || cell.getY() < 0 || cell.getY() > gridLength) {
+                System.err.println("Invalid input. Cell exceeds bounds of grid: " + cell);
+                System.exit(1);
+            }
+        }
     }
 }
